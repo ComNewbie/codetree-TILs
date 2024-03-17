1482cb9a8ce8e07cedf88e54cbc41092e038a334
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-03-17 17:27:51 +0900

    Title: 십의 자리 숫자, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240317/십의 자리 숫자/number-of-tens-digit.java
+++ 240317/십의 자리 숫자/number-of-tens-digit.java
@@ -3,14 +3,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        int size = 0;
+    
+        int[] arr = new int[100];
+    
+        int num = 0;
 
-        int[] arr = new int[10];
+        for(int i=0;true;i++){
+            num = scan.nextInt();
+            if(num == 0){
+                break;
+            }
 
-        for(int i=0;i<10;i++){
-            arr[(scan.nextInt()/10)]++;
+            else{
+                arr[(num/10)]++;
+                size++;
+            }
         }
-
-        for(int i=1;i<arr.length;i++){
+        
+        for(int i=1;i<=size;i++){
             System.out.println(i+" - "+arr[i]);
         }
     }
