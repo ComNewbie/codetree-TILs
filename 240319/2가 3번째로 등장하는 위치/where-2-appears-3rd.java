87b704edc3725f81233ce3f28e99d61b606deee2
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-03-19 16:16:59 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240319/2가 3번째로 등장하는 위치/where-2-appears-3rd.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        int[] arr = new int[n];
+
+        int count = 0;
+
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+            if(arr[i] == 2){
+                count++;
+            }
+            if(count == 3){
+                System.out.print(i+1);
+            }
+        }
+    }
+}
