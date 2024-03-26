a2e49ddd51aaba20573cde6b0039db0cc3c59b75
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-03-26 13:08:39 +0900

    Title: 두 개의 격자 비교하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240326/두 개의 격자 비교하기/compare-two-grid.java
+++ 240326/두 개의 격자 비교하기/compare-two-grid.java
@@ -12,14 +12,14 @@ public class Main {
 
         int num = 0;
 
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 arr[i][j] = scan.nextInt();
             }
         }
 
-         for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 num = scan.nextInt();
                 if(num == arr[i][j]){
                     System.out.print(0+" ");
