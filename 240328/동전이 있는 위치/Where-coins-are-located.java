f8b97e076e0c7f85cc2742803157d65af43fc9e5
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-03-28 13:43:38 +0900

    Title: 동전이 있는 위치, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240328/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        int m = scan.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        for(int i=0;i<m;i++){
+            arr[scan.nextInt()-1][scan.nextInt()-1] = 1;
+        
+        }
+        for(int[] e : arr){
+            for(int f : e){
+                System.out.print(f+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
