f3c69e23620a32a484b94bf40f88619c70a09ddf
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-04 14:00:47 +0900

    Title: 문자열 범위 출력하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240404/문자열 범위 출력하기 2/print-string-in-range-2.java
+++ 240404/문자열 범위 출력하기 2/print-string-in-range-2.java
@@ -9,7 +9,10 @@ public class Main {
         int n = scan.nextInt();
 
         if(n>str.length()){
-                System.out.print(str.reverse());
+                for(int i=str.length()-1;i>=0;i--){
+                    System.out.print(str.charAt(i));
+                }
+            }
         }
         else{
             for(int i=str.length()-1;i>=str.length()-n;i--){
