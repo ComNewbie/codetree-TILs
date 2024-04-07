b1042aff07d4079643ab7936912ce1876a2dd33d
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-07 16:41:33 +0900

    Title: 문자열 추가하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240407/문자열 추가하기/add-spring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.next();
+
+        System.out.print(str.concat("Hello"));
+
+    }
+}
