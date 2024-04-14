9bab6c80e13ac1bef19e3c8d1140ce969cc26de1
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-14 15:26:26 +0900

    Title: 문자열 계속 지우기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240414/문자열 계속 지우기/keep-removing-string.java
+++ 240414/문자열 계속 지우기/keep-removing-string.java
@@ -15,7 +15,7 @@ public class Main {
         while (modified) {
             modified = false; // modified 변수를 초기화
 
-            for (int i = 1; i < result.length(); i++) {
+            for (int i = 0; i < result.length()-1; i++) {
                 if (result.substring(i - (B.length()-1), i + (B.length()-1)).equals(B)) {
                     result = result.substring(0, i - (B.length()-1)) + result.substring(i + (B.length()-1));
                     modified = true; // result가 변경되었음을 표시
