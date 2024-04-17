1bc3516a824e48cf7e150ccb4ecea10ae86a8484
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-17 22:08:03 +0900

    Title: 규칙에 따라 밀기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240417/규칙에 따라 밀기/push-by-the-rules.java
+++ 240417/규칙에 따라 밀기/push-by-the-rules.java
@@ -11,22 +11,24 @@ public class Main {
         char[] arr = Command.toCharArray();
 
         int L = 0;
+        
         int R = 0;
+
         for(int i=0;i<arr.length;i++){
             if(arr[i] == 'L'){
                 L++;
             }
+
             if(arr[i] == 'R'){
                 R++;
             }
         }
+
         if(L > R){
-            System.out.print(A.substring((L-R))+A.substring(0,(L-R)));
+            System.out.print(A.substring((L-R)) + A.substring(0,(L-R)));
         }
         else{
-            System.out.print(A.substring(A.length()-(R-L))+A.substring(0,A.length()-(R-L)));
+            System.out.print(A.substring(A.length()-(R-L)) + A.substring(0,A.length()-(R-L)));
         }
-
-
     }
 }
