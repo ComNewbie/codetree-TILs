d88e87bd2c36214ec40d3478ea2b6bd28f985d0e
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-18 20:21:33 +0900

    Title: 아스키코드 변환, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240418/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        char a = scan.next().charAt(0);
+
+        int b = scan.nextInt();
+
+        System.out.print((int) a+" "+(char) b);
+
+    }
+}
