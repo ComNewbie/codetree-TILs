9750344060917a963d346098d83e88433e504549
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-18 20:19:48 +0900

    Title: 아스키코드표 맞추기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240418/아스키코드표 맞추기/chart-of-ASCII.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        for(int i=0;i<5;i++){
+            int num = scan.nextInt();
+
+            System.out.print((char) num+" ");
+        }
+
+    }
+}
