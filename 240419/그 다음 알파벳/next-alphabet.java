73fff5c3b544f0657414e6fd94a88991e2f1e552
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-19 21:22:52 +0900

    Title: 그 다음 알파벳, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240419/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        char alpha = scan.next().charAt(0);
+
+        System.out.print((char) ((int) alpha+1));        
+
+    }
+}
