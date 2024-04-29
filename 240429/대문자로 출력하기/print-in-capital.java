5d680379e285ae7f835904168e1cfc3bcb3db78f
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-29 12:39:21 +0900

    Title: 대문자로 출력하기, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240429/대문자로 출력하기/print-in-capital.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+
+        for(int i=0;i<str.length();i++){
+            char alpha = str.toUpperCase().charAt(i);
+             if(alpha >= 65){
+                System.out.print(alpha);
+             }
+        }
+    }
+}
