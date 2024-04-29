4120b929a59ea829434ebcabbfd822f2bf7fdbd7
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-29 12:47:40 +0900

    Title: 소문자와 숫자, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

--- 240429/소문자와 숫자/letter-and-number.java
+++ 240429/소문자와 숫자/letter-and-number.java
@@ -9,7 +9,7 @@ public class Main {
         for(int i=0;i<str.length();i++){
             char alpha = str.toLowerCase().charAt(i);
 
-            if((alpha >= 97 && alpha <= 122) || (alpha >= 49 && alpha <= 57)){
+            if((alpha >= 97 && alpha <= 122) || (alpha >= 48 && alpha <= 57)){
                 System.out.print(alpha);
             }
         }
