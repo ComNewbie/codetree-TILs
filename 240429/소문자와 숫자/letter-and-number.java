532a934e8b8ecf564822f70ea27bf5fa62f0e6dc
ComNewbie <88769991+ComNewbie@users.noreply.github.com>
2024-04-29 12:47:10 +0900

    Title: 소문자와 숫자, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240429/소문자와 숫자/letter-and-number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+
+        for(int i=0;i<str.length();i++){
+            char alpha = str.toLowerCase().charAt(i);
+
+            if((alpha >= 97 && alpha <= 122) || (alpha >= 49 && alpha <= 57)){
+                System.out.print(alpha);
+            }
+        }
+
+    }
+}
